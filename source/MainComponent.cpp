5b45c697b0befc9e75308c0405f387f7b9ed9f73
Rodney DuPlessis <rodney@rodneyduplessis.com>
2020-09-04 16:42:24 -0700

    added spectrogram component

--- source/MainComponent.cpp
+++ source/MainComponent.cpp
@@ -63,11 +63,11 @@ void MainComponent::timerCallback() {
 void MainComponent::paint(juce::Graphics& g) {
   // (Our component is opaque, so we must completely fill the background with a
   // solid colour)
-  g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+  // g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 
-  g.setFont(juce::Font(16.0f));
-  g.setColour(juce::Colours::white);
-  g.drawText("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+  // g.setFont(juce::Font(16.0f));
+  // g.setColour(juce::Colours::white);
+  // g.drawText("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized() {
